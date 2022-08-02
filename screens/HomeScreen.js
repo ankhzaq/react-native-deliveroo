f175d40422afc40cc4bc41973d455a9cd2fde5c2
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-08-02 18:50:22 +0200

    feature - Search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
-import { View, Text, SafeAreaView, Image } from 'react-native';
+import { View, Text, TextInput, SafeAreaView, Image } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { ChevronDownIcon, UserIcon } from 'react-native-heroicons/outline';
+import { AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserIcon } from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -15,7 +15,7 @@ const HomeScreen = () => {
   return (
     <SafeAreaView className="bg-white pt-5">
       {/* HEADER */}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
         <Image
           className='h-7 w-7 bg-gray-300 p-4 rounded-full'
           source={{
@@ -30,6 +30,18 @@ const HomeScreen = () => {
           </Text>
         </View>
         <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            keyboardType="default"
+            placeholder="Restaurants and cuisines"
+          />
+        </View>
+        <AdjustmentsIcon color="#00CCBB" />
       </View>
     </SafeAreaView>
   );
