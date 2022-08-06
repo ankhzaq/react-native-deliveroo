1b6c8a603ebfb2a6ee97bf930b51c80d71c99671
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-08-06 14:40:55 +0200

    feature - restaurantCard component and sanity folder added

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { Image, View, Text, TouchableOpacity } from 'react-native';
 import { LocationMarkerIcon, StarIcon } from 'react-native-heroicons/outline';
 import React from 'react';
 
@@ -15,20 +15,26 @@ const RestaurantCard = ({
   lat,
                         }) => {
   return (
-    <View className="px-3 pb-4">
-      <Text className="font-bold text-lg pt-2">{title}</Text>
-      <View className="flex-row items-center space-x-1">
-        <StarIcon color="green" opacity={0.5} size={22} />
-        <Text className="text-xs text-gray-500">
-          <Text className="text-green-500">{rating}</Text> - {genre}
-        </Text>
-      </View>
+    <TouchableOpacity className="bg-white mr-3 shadow">
+      <Image
+        className="h-36 w-64 rounded-sm"
+        source={{ uri: imgUrl}}
+      />
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <StarIcon color="green" opacity={0.5} size={22} />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-green-500">{rating}</Text> - {genre}
+          </Text>
+        </View>
 
-      <View className="flex-row items-center space-x-1">
-        <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
-        <Text className="text-xs text-gray-500">Nearby - {address}</Text>
+        <View className="flex-row items-center space-x-1">
+          <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+          <Text className="text-xs text-gray-500">Nearby - {address}</Text>
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
