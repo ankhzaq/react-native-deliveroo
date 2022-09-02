7abacc67cd2e9d1ba62ac265070eb985d5b25f0a
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-09-02 11:42:11 +0200

    feature - .idea restored and restaurantScreen structure added for the header

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,20 +1,42 @@
-import { View, Text, TextInput, SafeAreaView, ScrollView, Image } from 'react-native';
-import React, { } from 'react';
-import { useRoute } from '@react-navigation/native';
+import { View, Text, TextInput, SafeAreaView, ScrollView, Image, TouchableOpacity } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation, useRoute
+} from '@react-navigation/native';
+import { urlFor } from '../sanity';
+import { ArrowLeftIcon } from 'react-native-heroicons/solid';
 
 
 const RestaurantScreen = () => {
 
   const {
     params: {
+      image,
       name
     }
   } = useRoute();
 
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    });
+  }, [])
+
   return (
-    <View>
-      <Text>{name}</Text>
-    </View>
+    <ScrollView>
+      <View className="relative">
+        <Image
+          className="w-full h-56 bg-gray-300 p-4"
+          source={{
+            uri: urlFor(image).url(),
+          }}
+        />
+        <TouchableOpacity className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
+          <ArrowLeftIcon size={20} color="#00CCBB"  />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 }
 
