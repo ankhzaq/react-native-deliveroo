b8109618d0f94eaa8d9fdbccb0c07265d8992e00
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-09-03 12:57:32 +0200

    feature - Restaurant screen structure done

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -3,17 +3,32 @@ import React, { useLayoutEffect } from 'react';
 import { useNavigation, useRoute
 } from '@react-navigation/native';
 import { urlFor } from '../sanity';
-import { ArrowLeftIcon } from 'react-native-heroicons/solid';
+import {
+  ArrowLeftIcon,
+  ChevronRightIcon,
+  LocationMarkerIcon,
+  QuestionMarkCircleIcon,
+  StarIcon
+} from 'react-native-heroicons/solid';
 
 
 const RestaurantScreen = () => {
 
   const {
-    params: {
-      image,
-      name
-    }
+    params
   } = useRoute();
+
+  const {
+    address,
+    dishes,
+    genre,
+    image,
+    rating,
+    short_description,
+    name
+  } = params;
+
+  console.log("params: ", params);
 
   const navigation = useNavigation();
 
@@ -32,9 +47,50 @@ const RestaurantScreen = () => {
             uri: urlFor(image).url(),
           }}
         />
-        <TouchableOpacity className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
+        <TouchableOpacity
+          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
+          onPress={navigation.goBack}
+        >
           <ArrowLeftIcon size={20} color="#00CCBB"  />
         </TouchableOpacity>
+      </View>
+
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+          <Text className="text-3xl font-bold">{name}</Text>
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{rating}</Text> - {genre}
+              </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+              <Text className="text-xs text-gray-500">Nearby - {address}</Text>
+            </View>
+          </View>
+
+          <text className="text-gray-500 mt-2 pb-4">{short_description}</text>
+        </View>
+
+
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text className="pl-2 flex-1 text-md font-bold">Have a food allergy?</Text>
+          <ChevronRightIcon color="#00CCBB" />
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+        {
+          dishes.map((dish) => (
+            <Text>dish</Text>
+          ))
+        }
       </View>
     </ScrollView>
   );
