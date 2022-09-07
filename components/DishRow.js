505e98f85745062299b94adc4afbba8dc5025e31
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-09-07 19:23:56 +0200

    feature - quantity of dishes

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,33 +1,53 @@
 import { View, Text } from 'react-native';
 import Currency from "react-currency-formatter";
 import { Image, TouchableOpacity } from 'react-native-web';
-import React from 'react';
+import React, { useState } from 'react';
 import { urlFor } from '../sanity';
+import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid';
 
 const DishRow = (props) => {
+  const [isPressed, setIsPressed] = useState(false);
   const { name, short_description, price, image } = props;
   return (
-    <TouchableOpacity className="bg-white border p-4 border-gray-200">
-      <View className="flex-row">
-        <View className="flex-1 pr-2">
-          <Text className="text-lg mb-1">{name}</Text>
-          <Text className="text-gray-400">{short_description}</Text>
-          <Text className="text-gray-400 mt-2">
-            <Currency quantity={price} currency="GBP" />
-          </Text>
+    <>
+      <TouchableOpacity className="bg-white border p-4 border-gray-200" onPress={() => setIsPressed(!isPressed)}>
+        <View className="flex-row">
+          <View className="flex-1 pr-2">
+            <Text className="text-lg mb-1">{name}</Text>
+            <Text className="text-gray-400">{short_description}</Text>
+            <Text className="text-gray-400 mt-2">
+              <Currency quantity={price} currency="GBP" />
+            </Text>
+          </View>
+          <View>
+            <Image
+              style={{
+                borderWidth: 1,
+                borderColor: '#F3F3F4'
+              }}
+              className="h-20 w-20 bg-gray-300 p-4"
+              source={{ uri: urlFor(image).url() }}
+            />
+          </View>
         </View>
-        <View>
-          <Image
-            style={{
-              borderWidth: 1,
-              borderColor: '#F3F3F4'
-            }}
-            className="h-20 w-20 bg-gray-300 p-4"
-            source={{ uri: urlFor(image).url() }}
-          />
+      </TouchableOpacity>
+      {isPressed && (
+        <View className="bg-white px-4">
+          <View className="flex-row items-center space-x-2 pb-3">
+            <TouchableOpacity>
+              <MinusCircleIcon color="#00CCBB" size={40} />
+            </TouchableOpacity>
+
+            <Text>0</Text>
+
+            <TouchableOpacity>
+              <PlusCircleIcon color="#00CCBB" size={40} />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </TouchableOpacity> );
+      )}
+    </>
+  );
 };
 
 export default DishRow;
