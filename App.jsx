7676666ba250ba6d170c55cc6c5e227ba1ebb64d
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-09-12 21:24:22 +0200

    feature - basketScreen added

--- App.jsx
+++ App.jsx
@@ -8,6 +8,7 @@ import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import { Provider } from 'react-redux';
 import { store } from './store';
+import BasketScreen from './screens/BasketScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -20,6 +21,11 @@ export default function App() {
             { /* Screens */}
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+            <Stack.Screen
+              name="Basket"
+              component={BasketScreen}
+              options={{ presentation: "modal", headerShown: false }}
+            />
           </Stack.Navigator>
         </TailwindProvider>
       </Provider>
