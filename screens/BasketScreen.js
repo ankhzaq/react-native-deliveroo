d260b6b3b451350153cde765cb41dd923d4f0020
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-09-17 18:48:08 +0200

    BasketScreen styled

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,9 +1,10 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
 import React, { useMemo, useState } from 'react';
 import { useNavigation } from "@react-navigation/native";
 import { useDispatch, useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
 import { selectBasketItems } from '../features/basketSlice';
+import { XCircleIcon } from 'react-native-heroicons/solid';
 
 const BasketScreen = () => {
   const navigation = useNavigation();
@@ -22,9 +23,20 @@ const BasketScreen = () => {
   }, [items]);
 
   return (
-    <SafeAreaView>
-      <View>
-        Basket Screen
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b border-[#00CCBB] bg-white shadow-xs">
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">{restaurant.title}</Text>
+          </View>
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 absolute top-3 right-5"
+          >
+            <XCircleIcon color="#00CCBB" height={50} width={50} />
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
